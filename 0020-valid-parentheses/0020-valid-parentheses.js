6c063db8a358455223249ac5e1ee25c258888960
Alex Goldschmidt <83550948+alex-goldschmidt@users.noreply.github.com>
2023-02-27 11:34:27 -0500

    Time: 70 ms (61.45%), Space: 42.9 MB (25.91%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ 1. Create stack array
+ 2. create map object that matches opening and closing parentheses
+ 3. for loop
+        if opening parentheses in map, push closing parentheses on to stack
+        if closing parentheses in map, stack.pop(). if not, return false
+ 4. check if stack is empty via "return !stack.length"       
+ */
+var isValid = function(s) {
+    const stack = []
+    const map = {
+        "(" : ")",
+        "[" : "]",
+        "{" : "}"
+    } 
+    for (let i = 0; i < s.length; i++) {
+        let c = s[i]
+        if (map[c]) stack.push(map[c])
+        else if (c !== stack.pop()) return false
+    }
+    return !stack.length
+};
