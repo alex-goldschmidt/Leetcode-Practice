9b4b0707c918b445cc1798782b716947438397de
Alex Goldschmidt <83550948+alex-goldschmidt@users.noreply.github.com>
2023-03-30 12:11:38 -0400

    Time: 77 ms (88.26%), Space: 52.1 MB (18.00%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -1,14 +1,13 @@
 /**
  * @param {number[]} prices
  * @return {number}
- 1. create minPrice = first array element & maxProfit = 0
- 2. for loop
- 3. if current element is less than minPrice, set minPrice = current element
- 4. else, set maxProfit = current element - minPrice
- 5. return maxProfit
+ 1. create minPrice and maxProfit variables
+ 2. for loop over prices array
+ 3. if current element < minPrice, set minPrice = current element
+ 4. else, set maxProfix = Math.max(maxProfit, curr - minPrice)
  */
 var maxProfit = function(prices) {
-    let minPrice = prices[0];
+    let minPrice = prices[0]
     let maxProfit = 0
     for (let i = 0; i < prices.length; i++) {
         if (prices[i] < minPrice) minPrice = prices[i]
