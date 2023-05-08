80550b218d57f012b60d2db8b0046a4f8c9a77ee
Alex Goldschmidt <83550948+alex-goldschmidt@users.noreply.github.com>
2023-05-08 14:06:54 -0400

    Time: 51 ms (98.71%), Space: 43.1 MB (29.41%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -2,10 +2,6 @@
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
-  1. create hashmap
-  2. for loop
-  3. if diff in map, return its indice and the diff variable in the map
-  4. else, add current element to the map
  */
 var twoSum = function(nums, target) {
     let map = {}
